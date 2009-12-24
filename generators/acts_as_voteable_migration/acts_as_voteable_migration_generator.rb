06e2a514cb505c39c79c6e15310ef94ae8aa462f
Igor Sokolov <igor.sokolov@gmail.com>
2009-12-24 11:35:53 +0200

    added generator for migration

---
+++ generators/acts_as_voteable_migration/acts_as_voteable_migration_generator.rb
@@ -0,0 +1,13 @@
+class ActsAsVoteableMigrationGenerator < Rails::Generator::Base
+
+  def manifest
+    record do |m|
+      m.migration_template 'migration.rb', 'db/migrate'
+    end
+  end
+
+  def file_name
+    "acts_as_voteable_migration"
+  end
+
+end
